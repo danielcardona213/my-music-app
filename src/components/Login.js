a7daf065317a5db973db352fe51a1683b8539085
Daniel Cardona Calderon <daniel.cardona@PRAGMA.COM.CO>
2020-01-09 12:10:13 -0500

    Se anidaron nuevos estilos al login

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,25 +5,33 @@ import './Styles/Login.css'
 class Login extends React.Component{
     render(){
         return( 
-            <div className ="content">
-                <div className ="login"> 
-                    <img className="logo" src={logo}/>
-                    <form className="formu">
-                        <input className="texto" type="text" name="eMail" placeholder="Correo..." />
-                        <input className="texto" type="password" name="password" placeholder="Contraseña..." />
+            <body>
+                <div className ="content">
+                    <div className ="login"> 
+                        <img className="logo" src={logo}/>
                         
-                    </form>
-                    <div className="contbtn">
-                        <button className ="btn">Entrar</button>
+                            <h1>My music</h1>
+                            <h3>Login</h3>
+                        
+                        <form className="formu">
+                            
+                            <input className="texto" type="text" name="eMail" placeholder="Correo..." />
+                            <input className="texto" type="password" name="password" placeholder="Contraseña..." />
+                            
+
+                            <div className="contbtn">
+                            <button className ="btn">Entrar</button>
+                            </div>
+                        </form>
+                     
+                        <div className ="contbtn">
+                            <button className="btn">Registrase</button>
+                        </div>
                     </div>
-                    <div className ="contbtn">
-                        <button className="btn">Registrase</button>
-                    </div>
+
+
                 </div>
-
-
-            </div>
-
+            </body>
          );
     }
 
